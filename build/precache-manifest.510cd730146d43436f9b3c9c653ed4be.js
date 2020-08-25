5e001d4365d4d0e2ef344c1f1fe402608b6c208d
Shehzad Mahmood <65547678+ShehzadMahmood@users.noreply.github.com>
2020-08-25 23:16:05 +0500

    Add features for Add Transaction

---
+++ build/precache-manifest.510cd730146d43436f9b3c9c653ed4be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7534178046458b37f9187bf3d5a6a2f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5e3ba976ac508f9b24f",
+    "url": "/static/css/main.c896a4b2.chunk.css"
+  },
+  {
+    "revision": "48cb69548b1071b75b5e",
+    "url": "/static/js/2.2270564b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2270564b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5e3ba976ac508f9b24f",
+    "url": "/static/js/main.aad55f09.chunk.js"
+  },
+  {
+    "revision": "8b578aaf0d954a8e70ab",
+    "url": "/static/js/runtime-main.9cf76795.js"
+  }
+]);
